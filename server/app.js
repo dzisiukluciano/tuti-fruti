e6e627e1f27f11366d438b48ccdc02ee6cd4cb4d
Sebastian Gonzalez <sgonzalez@telectronica.telectronicapeaje.local>
2016-12-16 17:55:05 -0300

    added socket io

--- server/app.js
+++ server/app.js
@@ -1,59 +1,31 @@
-var express = require('express')
-  , app = express()
-  , sse = require('./sse')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var connections = []
-  , votes = {yes: 0, no: 0}
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+io.on('connection', function(socket){
+  console.log('a user connected');
 
-app.use(sse)
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 
-var time = 60;
-function timeout() {
-    setTimeout(function () {
-      for(var i = 0; i < connections.length; i++) {
-        connections[i].sseSendTimeout({
-            message: time
-            })
-        }
-        time -= 1;
-        if(time == 0)
-            time = 60;
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 
-        timeout();
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  socket.on('chat message', function(msg){
+    console.log("broadcast");
+    socket.broadcast.emit('alert message',msg);
+  });
+});
 
-    }, 1000);
-};
-
-timeout();
-
-app.get('/', function(req, res) {
-  res.render('vote')
-})
-
-app.get('/result', function(req, res) {
-  res.render('result')
-})
-
-app.get('/vote', function(req, res) {
-  if (req.query.yes === "true") votes.yes++
-  else votes.no++
-
-  for(var i = 0; i < connections.length; i++) {
-    connections[i].sseSendMessage(votes)
-  }
-
-  res.sendStatus(200)
-})
-
-app.get('/stream', function(req, res) {
-  res.sseSetup()
-  res.sseSendMessage(votes)
-  connections.push(res)
-})
-
-app.listen(3000, function() {
-  console.log('Listening on port 3000...')
-})
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
